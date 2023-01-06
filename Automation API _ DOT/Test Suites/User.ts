0d946d5db6b4dceffb657dcf0cf88e51459f1284
gilangpradd <53503695+gilangpradd@users.noreply.github.com>
2023-01-06 10:45:04 +0700

    Add files via upload

---
+++ Automation API _ DOT/Test Suites/User.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>User</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>91cf369e-b9c1-4e3e-bc4f-d500b1f374a1</testSuiteGuid>
+   <testCaseLink>
+      <guid>0acb3e34-5acb-418d-8533-e148f993c07c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/getuserdetail</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
